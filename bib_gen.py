e84bbb9953ddd1b8556c273e709bbebb45f725ee
antoniovj1 <antoniodelavegajimenez@gmail.com>
2015-11-28 20:19:22 +0100

    Eliminado genItem2, añadidos comentarios.

--- bib_gen.py
+++ bib_gen.py
@@ -7,6 +7,7 @@ import sys
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
 
+#Descarga el html y obtiene el titulo haciendo uso de re
 def getTitle(link):
   req = urllib2.Request(link, headers=hdr)
   html = urllib2.urlopen(req)
@@ -16,6 +17,7 @@ def getTitle(link):
   title = title[7:-8]
   return title
 
+#Obtiene la etiqueta ( lo que hay delante del ;)
 def getLabel(line):
     label_regex = re.search(r'(.*);', line, re.IGNORECASE)
     label = label_regex.group(0)
@@ -26,32 +28,33 @@ def getURL(line,label):
     url = line[len(label)+1:-1]
     return url
 
-#Añade el el caracter de escape \ delante de los caracteres especiales.
+#Añade el el carácter de escape \ delante de los caracteres especiales.
 def fixURL(url):
     url.replace("#","\#")
     url.replace("_","\_")
     url.replace("&","\&")
     return url
 
+#Genera una entrada completa de la bibliografía a partir de una linea del archivo_urls
 def genItem(line):
     label = getLabel(line)
     url = getURL(line,label)
     date = time.strftime("%d/%m/%Y")
-    title = getTitle(url)
     url = fixURL(url)
 
-    item = "@misc{" + label + ",\n\tauthor = {},\n\ttitle = {" + title +"},\n\tyear = {(Accedido el "\
-    + date + ")}, \n\thowpublished =\"\\url{"+url+"}\"\n} \n\n"
-    return item
+    try:
+        title = getTitle(url)
 
-def genItem2(line):
-    label = getLabel(line)
-    url = getURL(line,label)
-    date = time.strftime("%d/%m/%Y")
-    url = fixURL(url)
+        item = "@misc{" + label + ",\n\tauthor = {},\n\ttitle = {" + title +"},\n\tyear = {(Accedido el "\
+        + date + ")}, \n\thowpublished =\"\\url{"+url+"}\"\n} \n\n"
+    except Exception:
+        sys.exc_clear()
+        print ("Linea -> " + line + " generada sin título" )
 
-    item = "@misc{" + label + ",\n\tauthor = {},\n\ttitle = {},\n\tyear = {(Accedido el "\
-    + date + ")}, \n\thowpublished =\"\\url{"+url+"}\"\n} \n\n"
+        #En caso de no poder obtener el html, genera una entrada que NO incluye el título
+        item = "@misc{" + label + ",\n\tauthor = {},\n\ttitle = {},\n\tyear = {(Accedido el "\
+        + date + ")}, \n\thowpublished =\"\\url{"+url+"}\"\n} \n\n"
+
     return item
 
 if(len(sys.argv) == 3):
@@ -59,12 +62,7 @@ if(len(sys.argv) == 3):
     bib = open(sys.argv[2],"a")
 
     for line in urls:
-        try:
-            bib.write(genItem(line))
-        except Exception:
-            sys.exc_clear()
-            bib.write(genItem2(line))
-            print ("Linea -> " + line + " generada sin titulo" )
+        bib.write(genItem(line))
 
     urls.close()
     bib.close()
